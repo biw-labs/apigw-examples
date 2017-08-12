3ef7857b1adce4dae1c8ce5d21542733c509159d
dehnel <adam.dehnel@biworldwide.com>
2017-08-12 07:54:48 -0500

    Adjusted curl example showing a better POST example

--- curl/example.sh
+++ curl/example.sh
@@ -9,4 +9,8 @@ TOKEN_EXTENSION=/oauth2/token
 TOKEN_RESPONSE=$(curl -X POST -F "client_id=$APIGW_CLIENT_ID" -F "client_secret=$APIGW_CLIENT_SECRET" -F "grant_type=client_credentials" $APIGW_BASE_URL$RESOURCE_PATH$TOKEN_EXTENSION)
 ACCESS_TOKEN=$(echo $TOKEN_RESPONSE | python -c "import sys, json; print json.load(sys.stdin)['access_token']")
 
-curl -X POST $APIGW_BASE_URL$RESOURCE_PATH?access_token=$ACCESS_TOKEN
+# for GET endpoints this should do the trick
+curl $APIGW_BASE_URL$RESOURCE_PATH?access_token=$ACCESS_TOKEN
+
+# for POSTing and other methods include it in the header (which should work for GET as well)
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS_TOKEN" -d '{"test": "this"}' $APIGW_BASE_URL$RESOURCE_PATH
